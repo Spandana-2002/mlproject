b8f5e3e32f35987b829a0b1b03c44d0f56bb9991
Spandana-2002 <98699351+Spandana-2002@users.noreply.github.com>
2023-06-30 12:22:50 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -19,8 +19,8 @@ def get_requirements(file_path:str)->List[str]:
 setup(
 name='mlproject',
 version='0.0.1',
-author='Spandana',
-author_email='spandanareddy.mekapati@gmail.com',
+author='Krish',
+author_email='krishnaik06@gmail.com',
 packages=find_packages(),
 install_requires=get_requirements('requirements.txt')
 
